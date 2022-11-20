03ea6051e3fef708ab3c7ad3e019a4f6aa2ab451
useharm <cool.evsey@mail.ru>
2022-11-20 23:15:40 +0300

    fixed gallery imgs

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -8,40 +8,40 @@ const Gallery: React.FC = () => {
             <section className="all__marg">
     <div className="container">
         <div className="pages__gallery">
-            <a href={svgs['./pexels1.jpg']} className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels1.jpg']} className="gallery__box">
                 <img src={svgs['./pexels1.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels2.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels2.jpg']} className="gallery__box">
                 <img src={svgs['./pexels2.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels3.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels3.jpg']} className="gallery__box">
                 <img src={svgs['./pexels3.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels4.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels4.jpg']} className="gallery__box">
                 <img src={svgs['./pexels4.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels5.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels5.jpg']} className="gallery__box">
                 <img src={svgs['./pexels5.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels6.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels6.jpg']} className="gallery__box">
                 <img src={svgs['./pexels6.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels7.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels7.jpg']} className="gallery__box">
                 <img src={svgs['./pexels7.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels8.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels8.jpg']} className="gallery__box">
                 <img src={svgs['./pexels8.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels9.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels9.jpg']} className="gallery__box">
                 <img src={svgs['./pexels9.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels10.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels10.jpg']} className="gallery__box">
                 <img src={svgs['./pexels10.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels11.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels11.jpg']} className="gallery__box">
                 <img src={svgs['./pexels11.jpg']} alt="" />
             </a>
-            <a href="assets/img/pexels122.jpg" className="gallery__box" data-fancybox="gallery">
+            <a href={svgs['./pexels122.jpg']} className="gallery__box">
                 <img src={svgs['./pexels122.jpg']} alt="" />
             </a>
         </div>
